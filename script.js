a2cd8220da612edc2d82568930eaeb345397e199
Esteban-Jelly <tirauncable@gmail.com>
2025-09-08 09:47:09 -0400

    Update script.js 2025-09-08

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const lastUpdatedElement = document.getElementById('last-updated');
     const catalogListElement = document.getElementById('catalog-list');
 
-    const lastUpdatedDate = '01 de septiembre de 2025';
+    const lastUpdatedDate = '08 de septiembre de 2025';
     lastUpdatedElement.textContent = lastUpdatedDate;
 
     const catalogos = [
